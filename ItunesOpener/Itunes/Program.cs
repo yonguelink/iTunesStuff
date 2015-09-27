b25d8f31a827a4d5e33a36128506b599f05ce520
yonguelink <isaac.computing@gmail.com>
2015-09-27 12:44:26 -0400

    Added SDK && Opener

---
+++ ItunesOpener/Itunes/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Itunes
+{
+    class Program
+    {
+        public static iTunesLib.iTunesApp ituneApp;
+        static void Main(string[] args)
+        {
+            ituneApp = new iTunesLib.iTunesApp();
+            iTunesLib.IITSourceCollection sources = ituneApp.Sources;
+            iTunesLib.IITPlaylistCollection playlists = sources.ItemByName["Library"].Playlists;
+            iTunesLib.IITPlaylist lib = playlists.ItemByName[args[0]];
+            lib.Shuffle = true;
+            lib.PlayFirstTrack();
+            ituneApp.OnQuittingEvent += new iTunesLib._IiTunesEvents_OnQuittingEventEventHandler(onQuit);
+        }
+
+        private static void onQuit()
+        {
+            ituneApp.Quit();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # iTunesStuff
+
 Any script and/or code related to iTunes
+
+## /ItunesOpener
+
+Simple .exe that opens iTunes and start playing the given playlist's name (1st and only parameter)
+
+## /iTunesCOMWindowsSDK
+
+SDK given by Apple
